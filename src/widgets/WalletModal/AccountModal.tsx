1876229d7fea33dfdb7fac651a9823fe3f5d6254
Kevin McIntyre <kebin70@gmail.com>
2021-04-07 21:04:50 +0000

    change to avascan

--- src/widgets/WalletModal/AccountModal.tsx
+++ src/widgets/WalletModal/AccountModal.tsx
@@ -23,8 +23,8 @@ const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null
       {account}
     </Text>
     <Flex mb="32px">
-      <LinkExternal small href={`https://bscscan.com/address/${account}`} mr="16px">
-        View on BscScan
+      <LinkExternal small href={`https://avascan.info/blockchain/c/address/${account}`} mr="16px">
+        View on AvaScan
       </LinkExternal>
       <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
     </Flex>
